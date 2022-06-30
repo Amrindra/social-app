39f7f342fe7833e1c2a3e982888d791297354ba8
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-30 14:36:57 -0400

    Created Right, Left, and Feed components and added boilerplates to them

---
+++ src/component/leftNavbar/LeftNavbar.jsx
@@ -0,0 +1,7 @@
+import "./LeftNavbar.scss";
+
+const LeftNavbar = () => {
+  return <div>Left</div>;
+};
+
+export default LeftNavbar;
---
+++ src/component/rightNavbar/RightNavbar.jsx
@@ -0,0 +1,7 @@
+import "./RightNavbar.scss";
+
+const RightNavbar = () => {
+  return <div>Right</div>;
+};
+
+export default RightNavbar;
