609e634a22b196b00e62bd13b6314af52e21812d
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-06-29 20:26:58 -0400

    Gave styles to the portion of the TopNavbar.js

--- src/component/topNavbar/TopNavbar.jsx
+++ src/component/topNavbar/TopNavbar.jsx
@@ -5,34 +5,34 @@ const TopNavbar = () => {
   return (
     <div className="topNavbar_wrapper">
       <div className="topNavbar_left">
-        <span>DevFam</span>
+        <span className="logo">DevFam</span>
       </div>
 
       <div className="topNavbar_center">
         <div className="topNavbar_search">
-          <Search />
           <input className="search_input" placeholder="Search.." />
+          <Search className="search_icon" />
         </div>
       </div>
 
       <div className="topNavbar_right">
-        <div className="topNavbar_links">
-          <span className="topNavbar_link">Homepage</span>
-          <span className="topNavbar_link">Timeline</span>
+        <div className="topNavbar_right_links">
+          <span className="topNavbar_right_link">Homepage</span>
+          <span className="topNavbar_right_link">Timeline</span>
         </div>
 
-        <div className="topNavbar_icons">
-          <div className="topNavbar_icon_item">
+        <div className="topNavbar_right_icons">
+          <div className="topNavbar_right_icon_item">
             <Person />
-            <span className="topNavbar_icon_badge">1</span>
+            <span className="topNavbar_right_icon_badge">1</span>
           </div>
-          <div className="topNavbar_icon_item">
+          <div className="topNavbar_right_icon_item">
             <Chat />
-            <span className="topNavbar_icon_badge">1</span>
+            <span className="topNavbar_right_icon_badge">1</span>
           </div>
-          <div className="topNavbar_icon_item">
+          <div className="topNavbar_right_icon_item">
             <Notifications />
-            <span className="topNavbar_icon_badge">1</span>
+            <span className="topNavbar_right_icon_badge">1</span>
           </div>
         </div>
 
