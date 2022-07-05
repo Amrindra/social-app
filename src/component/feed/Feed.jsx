f4a14a6cb860de90e8392fa38eb13d4c288f24a4
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-07-05 12:01:05 -0400

    Import Share component to Feed component and added some React elements to Share com

--- src/component/feed/Feed.jsx
+++ src/component/feed/Feed.jsx
@@ -1,7 +1,14 @@
+import Share from "../share/Share";
 import "./Feed.scss";
 
 const Feed = () => {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feed_wrapper">
+        <Share />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
--- src/component/share/Share.jsx
+++ src/component/share/Share.jsx
@@ -1,7 +1,25 @@
 import "./Share.scss";
 
 const Share = () => {
-  return <div className="share">share</div>;
+  return (
+    <div className="share_container">
+      <div className="share_wrapper">
+        <div className="share_top">
+          <img
+            src="https://github.com/safak/youtube/blob/react-social-ui/public/assets/person/1.jpeg?raw=true"
+            alt="share profile"
+            className="share_profile_img"
+          />
+          <input
+            type="text"
+            placeholder="What's on your mind?"
+            className="share_input"
+          />
+        </div>
+      
+      </div>
+    </div>
+  );
 };
 
 export default Share;
