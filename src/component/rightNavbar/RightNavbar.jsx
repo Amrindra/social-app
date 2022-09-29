e91fa3f308fe4ebe9cc79a0a5a73213efb69c655
Amrindra <amrindraouk@yahoo.com>
2022-09-29 00:59:59 -0400

    implemented the online friends section

--- src/component/rightNavbar/RightNavbar.jsx
+++ src/component/rightNavbar/RightNavbar.jsx
@@ -13,6 +13,47 @@ const RightNavbar = () => {
           </ul>
         </div>
       </div>
+
+      <div className="rightBar_online_wrapper">
+        <h4 className="online_friend_title">Online</h4>
+        <ul className="online_friend_list">
+          <li className="online_friend">
+            <div className="online_friend_profile_img">
+              <img
+                src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                alt=""
+                className="profile_img"
+              />
+              <span className="online_green_dot"></span>
+            </div>
+            <div className="online_friend_name">John Doe</div>
+          </li>
+
+          <li className="online_friend">
+            <div className="online_friend_profile_img">
+              <img
+                src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                alt=""
+                className="profile_img"
+              />
+              <span className="online_green_dot"></span>
+            </div>
+            <div className="online_friend_name">John Doe</div>
+          </li>
+
+          <li className="online_friend">
+            <div className="online_friend_profile_img">
+              <img
+                src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                alt=""
+                className="profile_img"
+              />
+              <span className="online_green_dot"></span>
+            </div>
+            <div className="online_friend_name">John Doe</div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
