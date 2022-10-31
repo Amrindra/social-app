2d6b5b039a298b5c9f70633aed7c8c459ae3f01b
Amrindra <amrindraouk@yahoo.com>
2022-10-31 01:12:13 -0400

    Online users now are being fetched dynamically

--- src/component/rightNavbar/OnlineFriend.jsx
+++
@@ -1,7 +0,0 @@
-import "./OnlineFriend.scss";
-
-const OnlineFriend = () => {
-  return <div>OnlineFriend</div>;
-};
-
-export default OnlineFriend;
--- src/component/rightNavbar/RightNavbar.jsx
+++ src/component/rightNavbar/RightNavbar.jsx
@@ -19,16 +19,12 @@ const RightNavbar = () => {
         <h4 className="online_friend_title">Online</h4>
         {userData.map((user) => (
           <ul className="online_friend_list">
-            <li className="online_friend">
+            <li className="online_friend" key={user.id}>
               <div className="online_friend_profile_img">
-                <img
-                  src="https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
-                  alt=""
-                  className="profile_img"
-                />
+                <img src={user.profilePicture} alt="" className="profile_img" />
                 <span className="online_green_dot"></span>
               </div>
-              <div className="online_friend_name">John Doe</div>
+              <div className="online_friend_name">{user.username}</div>
             </li>
           </ul>
         ))}
