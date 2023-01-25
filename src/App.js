6d7c31672770eef05d890502caebea53fcf6e8a6
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2023-01-25 10:40:23 -0500

    Created the Profile page and built the foundation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Homepage from "./pages/homepage/Homepage";
 import Profile from "./pages/profile/Profile";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="app">
-      {/* <Homepage /> */}
       <Profile />
     </div>
   );
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,17 +2,43 @@ import Feed from "../../component/feed/Feed";
 import LeftNavbar from "../../component/leftNavbar/LeftNavbar";
 import RightNavbar from "../../component/rightNavbar/RightNavbar";
 import TopNavbar from "../../component/topNavbar/TopNavbar";
+import "./Profile.scss";
 
 const Profile = () => {
   return (
     <>
       <TopNavbar />
 
-      <section className="homepage_container">
+      <div className="profile_container">
         <LeftNavbar />
-        <Feed />
-        <RightNavbar />
-      </section>
+
+        <div className="profile_right">
+          <div className="profile_right_top">
+            <div className="profile_cover">
+              <img
+                className="profile_coverImg"
+                src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+                alt=""
+              />
+              <img
+                className="profile_userImg"
+                src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60g"
+                alt=""
+              />
+            </div>
+
+            <div className="profile_info">
+              <h4 className="profile_info_name">Amrin</h4>
+              <span className="profile_bio">What's up fam!</span>
+            </div>
+          </div>
+
+          <div className="profile_right_bottom">
+            <Feed />
+            <RightNavbar />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
