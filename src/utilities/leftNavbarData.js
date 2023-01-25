24e5f0f674a554ea351073aa19d982fe8323d9b0
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2023-01-25 12:36:53 -0500

    Moved listItems data from LeftNavbar component to utilities folder

---
+++ src/utilities/leftNavbarData.js
@@ -0,0 +1,29 @@
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School
+} from "@material-ui/icons";
+
+export const listItems = [
+  { icon: <RssFeed className="leftNavbar_icon" />, listName: "Feed" },
+  { icon: <Chat className="leftNavbar_icon" />, listName: "Chats" },
+  {
+    icon: <PlayCircleFilledOutlined className="leftNavbar_icon" />,
+    listName: "Video"
+  },
+  { icon: <Group className="leftNavbar_icon" />, listName: "Groups" },
+  { icon: <Bookmark className="leftNavbar_icon" />, listName: "Bookmarks" },
+  {
+    icon: <HelpOutline className="leftNavbar_icon" />,
+    listName: "Questions"
+  },
+  { icon: <WorkOutline className="leftNavbar_icon" />, listName: "Jobs" },
+  { icon: <Event className="leftNavbar_icon" />, listName: "Events" },
+  { icon: <School className="leftNavbar_icon" />, listName: "Courses" }
+];
