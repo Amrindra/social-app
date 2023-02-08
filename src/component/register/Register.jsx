8a0070830bd971aa0fb6e76b56855a1999d18b5d
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2023-02-08 10:43:22 -0500

    Created Register form and added Formik and Yub validation

---
+++ src/component/register/Register.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const RegisterForm = () => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  };
+
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    lastName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Required")
+  });
+
+  const onSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      console.log(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <Field type="text" name="firstName" placeholder="First Name" />
+            <ErrorMessage name="firstName" component="div" />
+          </div>
+          <div>
+            <Field type="text" name="lastName" placeholder="Last Name" />
+            <ErrorMessage name="lastName" component="div" />
+          </div>
+          <div>
+            <Field type="email" name="email" placeholder="Email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <div>
+            <Field type="password" name="password" placeholder="Password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default RegisterForm;
